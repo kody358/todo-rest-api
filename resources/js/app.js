6fe8964b439b607ace858523024a8e1cec03d1a5
kody <kody.dev358@gmail.com>
2025-07-05 13:34:40 +0900

    VueとTailwindCSSを導入

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
 import './bootstrap';
+import { createApp } from 'vue';
+import TodoApp from './components/TodoApp.vue';
+
+// Vueアプリケーションを作成
+const app = createApp({});
+
+// TodoAppコンポーネントをケバブケースで登録
+app.component('todo-app', TodoApp);
+
+// #appエレメントにマウント
+app.mount('#app');
